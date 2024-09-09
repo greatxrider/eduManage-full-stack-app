a6b094f575fe84cef4333e7942a54f8ece5ad957
greatxrider <daligdig.jephmari@gmail.com>
2024-09-10 02:41:15 +0800

    Add UserSignUp component

--- client/src/components/UserSignUp.jsx
+++ client/src/components/UserSignUp.jsx
@@ -1,5 +1,110 @@
-const UserSignUp = () => {
+import { useContext, useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { api } from '../utils/apiHelper';
 
+import UserContext from '../context/UserContext';
+import ErrorsDisplay from './ErrorsDisplay';
+
+const UserSignUp = () => {
+    const { actions } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    // State
+    const firstName = useRef(null);
+    const lastName = useRef(null);
+    const emailAddress = useRef(null);
+    const password = useRef(null);
+
+    const [errors, setErrors] = useState([]);
+
+    // event handlers
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const user = {
+            firstName: firstName.current.value,
+            lastName: lastName.current.value,
+            emailAddress: emailAddress.current.value,
+            password: password.current.value
+        }
+
+        try {
+            const response = await api("/users", "POST", user);
+            if (response.status === 201) {
+                console.log(`${user.firstName} is successfully signed up!`);
+                await actions.signIn({
+                    username: user.emailAddress,
+                    password: user.password
+                });
+                navigate("/");
+            } else if (response.status === 400) {
+                const data = await response.json();
+                setErrors(data.errors);
+            } else {
+                throw new Error('Unexpected response from server');
+            }
+        } catch (error) {
+            console.log(error);
+            navigate("/error");
+        }
+    }
+
+    const handleCancel = (event) => {
+        event.preventDefault();
+        navigate("/");
+    }
+
+    return (
+        <main>
+            <div className="form--centered">
+                <h2>Sign Up</h2>
+                <ErrorsDisplay errors={errors} />
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                        id="firstName"
+                        name="firstName"
+                        type="text"
+                        ref={firstName}
+                        placeholder="First Name"
+                    />
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                        id="lastName"
+                        name="lastName"
+                        type="text"
+                        ref={lastName}
+                        placeholder="Last Name"
+                    />
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input
+                        id="emailAddress"
+                        name="emailAddress"
+                        type="email"
+                        ref={emailAddress}
+                        placeholder="Email Address"
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        ref={password}
+                        placeholder="Password"
+                    />
+                    <button className="button" type="submit">Sign Up</button>
+                    <button
+                        className="button button-secondary"
+                        type="button"
+                        onClick={handleCancel}
+                    >
+                        Cancel
+                    </button>
+                </form>
+                <p>Already have a user account? Click here to <Link to="/signin">sign in</Link>!</p>
+            </div>
+        </main>
+    );
 }
 
 export default UserSignUp;
