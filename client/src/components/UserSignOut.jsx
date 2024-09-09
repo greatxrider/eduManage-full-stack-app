e97472978bce38c49e4c5cbac633197aa1eca8a2
greatxrider <daligdig.jephmari@gmail.com>
2024-09-10 02:40:52 +0800

    Add UserSignOut component

--- client/src/components/UserSignOut.jsx
+++ client/src/components/UserSignOut.jsx
@@ -1,5 +1,15 @@
-const UserSignOut = () => {
+import { useEffect, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import UserContext from '../context/UserContext';
 
-}
+const UserSignOut = () => {
+    const { actions } = useContext(UserContext);
+
+    useEffect(() => actions.signOut());
+
+    return (
+        <Navigate to="/" replace />
+    );
+};
 
 export default UserSignOut;
