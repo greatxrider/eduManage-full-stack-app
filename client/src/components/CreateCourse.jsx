fed7bef0519ea55bd9ed11d05e36146eeb71ceff
greatxrider <daligdig.jephmari@gmail.com>
2024-09-10 02:41:58 +0800

    Add CreateCourse component

--- client/src/components/CreateCourse.jsx
+++ client/src/components/CreateCourse.jsx
@@ -1,5 +1,98 @@
-const CreateCourse = () => {
+import { useContext, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { api } from '../utils/apiHelper';
 
+import UserContext from '../context/UserContext';
+import ErrorsDisplay from './ErrorsDisplay';
+
+const CreateCourse = () => {
+    const { authUser } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    // State
+    const title = useRef(null);
+    const description = useRef(null);
+    const estimatedTime = useRef(null);
+    const materialsNeeded = useRef(null);
+
+    const [errors, setErrors] = useState([]);
+
+    // event handlers
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const user = {
+            username: authUser.emailAddress,
+            password: authUser.password
+        }
+
+        const course = {
+            title: title.current.value,
+            description: description.current.value,
+            estimatedTime: estimatedTime.current.value,
+            materialsNeeded: materialsNeeded.current.value
+        }
+
+        try {
+            const response = await api("/courses", "POST", course, user);
+            if (response.status === 201) {
+                console.log(`${course.title} is successfully created!`);
+                navigate("/");
+            } else if (response.status === 400) {
+                const data = await response.json();
+                setErrors(data.errors);
+            } else {
+                throw new Error('Unexpected response from server');
+            }
+        } catch (error) {
+            console.log(error);
+            navigate("/error");
+        }
+    }
+
+    const handleCancel = (event) => {
+        event.preventDefault();
+        navigate("/");
+    }
+
+    return (
+        <main>
+            <div className="wrap">
+                <h2>Create Course</h2>
+                <ErrorsDisplay errors={errors} />
+                <form onSubmit={handleSubmit}>
+                    <div className="main--flex">
+                        <div>
+                            <label htmlFor="courseTitle">Course Title</label>
+                            <input id="courseTitle" name="courseTitle"
+                                type="text" ref={title} />
+                            <p>By {authUser.firstName} {authUser.lastName}</p>
+
+                            <label htmlFor="courseDescription">Course
+                                Description</label>
+                            <textarea id="courseDescription"
+                                name="courseDescription" ref={description}></textarea>
+                        </div>
+                        <div>
+                            <label htmlFor="estimatedTime">Estimated
+                                Time</label>
+                            <input id="estimatedTime" name="estimatedTime"
+                                type="text" ref={estimatedTime} />
+
+                            <label htmlFor="materialsNeeded">Materials
+                                Needed</label>
+                            <textarea id="materialsNeeded"
+                                name="materialsNeeded" ref={materialsNeeded}></textarea>
+                        </div>
+                    </div>
+                    <button className="button" type="submit">Create
+                        Course</button><button
+                            className="button button-secondary"
+                            onClick={handleCancel}>Cancel</button>
+                </form>
+            </div>
+        </main>
+    );
 }
 
 export default CreateCourse;
