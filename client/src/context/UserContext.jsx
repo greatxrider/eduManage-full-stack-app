1c8c758ccec964298f2ba6fee2be394d9f46c287
greatxrider <daligdig.jephmari@gmail.com>
2024-09-10 20:42:33 +0800

    Add UserContext with saving password temporarily

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -6,39 +6,47 @@ const UserContext = createContext(null);
 
 export const UserProvider = (props) => {
     const cookie = Cookies.get("authenticatedUser");
+    const cookiePassword = Cookies.get("password");
     const [authUser, setAuthUser] = useState(cookie ? JSON.parse(cookie) : null);
+    const [password, setPassword] = useState(cookiePassword ? JSON.parse(cookiePassword) : null);
 
     const signIn = async (credentials) => {
-        const response = await api("/users", "GET", null, credentials)
+        const response = await api("/users", "GET", null, credentials);
         if (response.status === 200) {
             const user = await response.json();
             setAuthUser(user);
+            setPassword(credentials.password);
             Cookies.set("authenticatedUser", JSON.stringify(user), { expires: 1 });
+            Cookies.set("password", JSON.stringify(credentials.password), { expires: 1 });
             return user;
         } else if (response.status === 401) {
             return null;
         } else {
             throw new Error();
         }
-    }
+    };
 
     const signOut = () => {
         setAuthUser(null);
+        setPassword(null);
         Cookies.remove("authenticatedUser");
+        Cookies.remove("password");
         Cookies.remove("defaultTheme");
-    }
+    };
 
     return (
         <UserContext.Provider value={{
             authUser,
+            password,
             actions: {
                 signIn,
-                signOut
+                signOut,
+                setPassword
             }
         }}>
             {props.children}
         </UserContext.Provider>
     );
-}
+};
 
 export default UserContext;
